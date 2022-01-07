6645264969d2caedbba214fae9200f9d34959663
binHussein <haddad.alglob@hotmail.com>
2022-01-07 17:38:11 +1300

    fix spelling of feelslike

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -25,7 +25,7 @@ const forecast = (lat,lon,callback)=>{
         else {
             callback(undefined, body.current.weather_descriptions[0] + 
                 ". It is currently " + body.current.temperature + 
-                " degrees out. It feels like " + body.current.feelsLike + 
+                " degrees out. It feels like " + body.current.feelslike + 
                 " degrees out. The humidity is " + body.current.humidity + 
                 "%."
                  )
